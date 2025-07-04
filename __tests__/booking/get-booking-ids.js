636005ff81a9ca6f5349a7384b656121f195cd27
ekawork25 <ekanovitawork24@gmail.com>
2025-07-04 19:11:31 +0700

    push get booking id

---
+++ __tests__/booking/get-booking-ids.js
@@ -0,0 +1,34 @@
+
+const getBookings = require('../../collections/booking/get-booking-ids');
+
+const testDataDir = __filename.split('.js')[0].replace('__tests__', 'test-data');
+
+const logger = require('../../utilities/logger');
+
+let testData = require('require-all')({
+    dirname: testDataDir
+});
+
+let res;
+
+describe('test get booking', () => {
+        test.each(Object.values(testData))(
+            "Test $title ", async ({title, header, body, expected_result }) => {
+                res = await getBookings.getBooking();
+                
+                // logger.logRequest(res);
+                // logger.logResponse(res);
+                // logger.logExpectation(res);
+            
+
+                expect(res.statusCode).toEqual(expected_result.status_code);
+                res.body.forEach(item => {
+                    expect(item).toHaveProperty(expected_result.expected_field);
+                });
+            
+            });
+    
+});
+
+
+
---
+++ collections/booking/get-booking-ids.js
@@ -0,0 +1,21 @@
+const restApiCaller = require('../../callers/rest-api');
+
+const endpointGetBooking ="/booking";;
+
+async function getBooking() {
+    const caller = new restApiCaller({
+        url: __URL__,
+        endPoint: endpointGetBooking,
+        header: {
+            "Content-Type": "application/json"
+        },
+        //body:body
+    })
+    const res = await caller.get()
+    return res
+}
+
+
+module.exports={
+    getBooking
+};
